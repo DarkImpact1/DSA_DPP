991ceacab285327fae9cb8909815192a1b7f2b22
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2023-09-27 00:30:55 +0530

    Update Sita.cpp

--- Lec10/Sita.cpp
+++ Lec10/Sita.cpp
@@ -1,73 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 struct Node {
     int data;
-    struct Node* next;
+    Node* next;
 };
 
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+void insertNode(Node** head, int data) {
+    Node* newNode = new Node();
     newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+    newNode->next = nullptr;
+
+    if (*head == nullptr) {
+        *head = newNode;
+    } else {
+        Node* temp = *head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
 }
 
-void insertNode(struct Node** head, int data) {
-    struct Node* newNode = createNode(data);
-    if (*head == NULL) {
-        *head = newNode;
+void removeDuplicates(Node* head) {
+    if (head == nullptr) {
         return;
     }
-    struct Node* current = *head;
-    while (current->next != NULL) {
-        current = current->next;
-    }
-    current->next = newNode;
-}
 
-void removeDuplicates(struct Node* head) {
-    struct Node* current = head;
-    while (current != NULL && current->next != NULL) {
+    Node* current = head;
+
+    while (current->next != nullptr) {
         if (current->data == current->next->data) {
-            struct Node* temp = current->next;
-            current->next = temp->next;
-            free(temp);
+            Node* duplicateNode = current->next;
+            current->next = current->next->next;
+            delete duplicateNode;
         } else {
             current = current->next;
         }
     }
 }
 
-void printLinkedList(struct Node* head) {
-    struct Node* current = head;
-    while (current != NULL) {
-        printf("%d ", current->data);
-        current = current->next;
+void displayLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
-    printf("\n");
+    cout << endl;
+}
+
+void deleteLinkedList(Node* head) {
+    Node* temp;
+    while (head != nullptr) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    struct Node* head = NULL;
-    for (int i = 0; i < n; i++) {
-        int data;
-        scanf("%d", &data);
-        insertNode(&head, data);
+    Node* head = nullptr;
+    int size;
+
+    cin >> size;
+
+    for (int i = 0; i < size; i++) {
+        int value;
+        cin >> value;
+        insertNode(&head, value);
     }
 
-    printf("Original Linked List: ");
-    printLinkedList(head);
+    cout << "Original Linked List: ";
+    displayLinkedList(head);
 
     removeDuplicates(head);
 
-    printf("Linked List after removing duplicates: ");
-    printLinkedList(head);
+    cout << "Linked List after removing duplicates: ";
+    displayLinkedList(head);
+
+    deleteLinkedList(head);
 
     return 0;
 }
-
-
-//Sushant
